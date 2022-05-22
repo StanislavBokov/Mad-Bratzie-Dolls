da83d52aee624cc99ce47532f4c748306259b1dc
StanislavBudin <zerrozauros96@list.ru>
2022-05-22 17:01:02 +0300

    create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import { Button } from './components/Button';
+import { MetamaskLogo } from './assets/icons';
 function App () {
   return (
-    <div className="App" />
+    <div className="App">
+      <Button size="lg" variant="outlined">
+        {/* <img src={MetamaskLogo} /> */}
+        Connect Wallet
+      </Button>
+    </div>
   );
 }
 
