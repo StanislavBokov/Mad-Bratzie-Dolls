77d39221751ac805510bd09c8713677429edb06e
StanislavBudin <zerrozauros96@list.ru>
2022-05-22 12:01:35 +0300

    init project

---
+++ src/components/types/components/typography.ts
@@ -0,0 +1,50 @@
+enum ColorValues {
+  default,
+  secondary,
+  error,
+  lightGreen,
+  green,
+  greenDark,
+  orange,
+  gray,
+  black,
+}
+
+enum SizeValues {
+  exS,
+  s,
+  m,
+  sm,
+  l,
+  sl,
+  f28,
+  f36,
+}
+
+enum WeightValues {
+  light,
+  regular,
+  medium,
+  semiBold,
+  bold,
+  extraBold,
+}
+
+enum Aligns {
+  center,
+  left,
+  right,
+}
+
+enum HeadingTypes {
+  h1,
+  h2,
+  h3,
+  h4,
+}
+
+export type TextColor = keyof typeof ColorValues;
+export type TextSize = keyof typeof SizeValues;
+export type TextWeight = keyof typeof WeightValues;
+export type TextAlign = keyof typeof Aligns;
+export type HeadingType = keyof typeof HeadingTypes;
