22b3ac82c084b9548477edca075fd1b38758c0f9
StanislavBudin <zerrozauros96@list.ru>
2022-05-24 17:26:29 +0300

    add setTimeOut

--- src/components/Mint/mint.tsx
+++ src/components/Mint/mint.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import { H3, Card, Text, Button } from "../index";
-import { firstDoll } from "../../assets/img";
+import { arrayOfDolls } from "../../assets/img";
 import styles from './styles.module.scss';
 
 export const Mint:React.FC = () => {
+  const [indexDoll, setIndexDoll] = useState(0);
   const [quantity, setQuantity] = useState(1);
+
+  // setTimeout(() => {
+  //   if(indexDoll === arrayOfDolls.length -1) {
+  //     setIndexDoll(0);
+  //   } else {
+  //     setIndexDoll(indexDoll + 1);
+  //   }
+  // },500);
+  
+  
   const handleClickMinus = () => {
     if(quantity > 1) {
       setQuantity(prevState => prevState - 1);
@@ -15,11 +26,10 @@ export const Mint:React.FC = () => {
     if(quantity < 40) {
       setQuantity(prevState => prevState + 1);
     }
-  
   };
   return (
     <Card className={styles.Mint}>
-      <img src={firstDoll} alt="" className={styles.imgDoll}/>    
+      <img src={arrayOfDolls[indexDoll]} alt="" className={styles.imgDoll}/>    
       <div className={styles.profilePic}>
         <H3 className={styles.title}>Profile Pic</H3>
         <Card size="sm" className={styles.cardPrice}>
